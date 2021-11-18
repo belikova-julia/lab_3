276f0642ad198026ecb0649a97325660be534794
belikova-julia <alistami38@gmail.com>
2021-11-18 22:03:21 +0300

    Scripted auto-commit on change (2021-11-18 22:03:21) by gitwatch.sh

--- src/main/java/iu9/AnalyzeAirportsApp.java
+++ src/main/java/iu9/AnalyzeAirportsApp.java
@@ -5,11 +5,14 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple1;
+import scala.Tuple2;
 
 import java.util.Map;
 
 public class AnalyzeAirportsApp {
     private static final String DELIMETER = ",";
+    private static final int AIRPORT_ID_COL = 0;
+    private static final int AIRPORT_NAME_COL = 1;
 
 
     public static void main(String[] args) throws Exception {
@@ -22,7 +25,7 @@ public class AnalyzeAirportsApp {
         Map<String, String> airportMap = airportFile
                 .filter(s -> !s.contains("Code"))
                 .map(s -> RowSerializable.parseRow(s, DELIMETER))
-                .mapToPair(s -> new Tuple1<>())
+                .mapToPair(row -> new Tuple2<>(row.getValue()))
                 .collectAsMap();
         final Broadcast<Map<String, String>> airportBrodcasted =
                 sc.broadcast(airportMap)
