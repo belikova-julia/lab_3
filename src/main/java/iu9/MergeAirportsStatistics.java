ff5cf467960c570baf9f9dedce39928ac61ec697
belikova-julia <alistami38@gmail.com>
2021-11-18 23:13:11 +0300

    Scripted auto-commit on change (2021-11-18 23:13:11) by gitwatch.sh

--- src/main/java/iu9/MergeAirportsStatistics.java
+++ src/main/java/iu9/MergeAirportsStatistics.java
@@ -2,8 +2,6 @@ package iu9;
 
 import org.apache.spark.api.java.function.Function2;
 
-import java.util.function.Function;
-
 public class MergeAirportsStatistics implements
         Function2<AnalyticsSerializable, AnalyticsSerializable, AnalyticsSerializable> {
     @Override
