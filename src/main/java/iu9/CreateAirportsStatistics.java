b4d89892c01f215893b4f268c71f282d92b7c170
belikova-julia <alistami38@gmail.com>
2021-11-18 22:59:55 +0300

    Scripted auto-commit on change (2021-11-18 22:59:55) by gitwatch.sh

--- src/main/java/iu9/CreateAirportsStatistics.java
+++ src/main/java/iu9/CreateAirportsStatistics.java
@@ -3,6 +3,14 @@ package iu9;
 import java.util.function.Function;
 
 public class CreateAirportsStatistics implements Function<String, AnalyticsSerializable> {
-    public AnalyticsSerializable call(String )
+    public AnalyticsSerializable call(String delay) throws Exception {
+        if (delay.isEmpty()) {
+            return new AnalyticsSerializable(0F, 0, 1, 1);
+        }
+        if (Float.parseFloat(delay) != 0F) {
+            return new AnalyticsSerializable(Float.parseFloat(delay), 1, 0, 1);
+        }
+        return new AnalyticsSerializable(0F, 0, 0, 1);
+    }
 
 }
