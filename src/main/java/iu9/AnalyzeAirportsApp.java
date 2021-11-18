b4442d92bf05bf89570a4b0f2a238324f9a01598
belikova-julia <alistami38@gmail.com>
2021-11-18 21:18:26 +0300

    Scripted auto-commit on change (2021-11-18 21:18:26) by gitwatch.sh

--- src/main/java/iu9/AnalyzeAirportsApp.java
+++ src/main/java/iu9/AnalyzeAirportsApp.java
@@ -2,6 +2,7 @@ package iu9;
 
 public class AnalyzeAirportsApp {
     public static void main(String[] args) throws Exception {
-        
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
