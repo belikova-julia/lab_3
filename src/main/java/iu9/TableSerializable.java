93dc94c85ba644c3b8e663e0fe7141628fac3f02
belikova-julia <alistami38@gmail.com>
2021-11-18 21:52:45 +0300

    Scripted auto-commit on change (2021-11-18 21:52:45) by gitwatch.sh

--- src/main/java/iu9/TableSerializable.java
+++ src/main/java/iu9/TableSerializable.java
@@ -3,6 +3,9 @@ package iu9;
 import java.io.Serializable;
 
 public class TableSerializable implements Serializable {
+
+    private RowSerializable(String[] )
+
     public static TableSerializable parseRow(String row, String del) {
         return new TableSerializable(
                 row.substring(1, row.length()-1).split("\""+del+"\"")
